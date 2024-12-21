9d1e7a9152f2752eb76d6380320631d8c7ade4cb
shenkzjay <senksjay@gmail.com>
2024-12-21 17:20:35 +0100

    fixing deployment issues

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,6 +1,5 @@
 import { redirect } from "next/navigation";
 import { GetUsers } from "../api/queries/getUser";
-import Link from "next/link";
 
 export default async function HomeAdminDashboard() {
   const user = await GetUsers();
