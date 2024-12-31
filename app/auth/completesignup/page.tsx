4fd21d9cf66b52a0019e9689994ec4d5a6e6ecbc
shenkzjay <senksjay@gmail.com>
2024-12-31 16:28:21 +0100

    still fixing deployment isuues

--- app/auth/completesignup/page.tsx
+++ app/auth/completesignup/page.tsx
@@ -1,9 +1,7 @@
-"use server";
-
 import { CompleteSignUp } from "./complete-signup";
 import { Suspense } from "react";
 
-export default function Page() {
+export default async function Page() {
   return (
     <div className="logbg w-full h-screen flex flex-col justify-center items-center">
       <div className="bg-white p-10 rounded-[32px] md:w-1/3 mx-6 md:mx-0">
--- app/dashboard/resident/payment/page.tsx
+++ app/dashboard/resident/payment/page.tsx
@@ -1,5 +1,3 @@
-"use server";
-
 import { GetSingleResident } from "@/app/api/queries/get-single-residents";
 import { DashPayment } from "../dashboard/dash-payment";
 import { GetUsers } from "@/app/api/queries/getuser-session";
