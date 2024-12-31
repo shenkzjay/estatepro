dc97c278c07a546ba5885833b686488a1ed87b79
shenkzjay <senksjay@gmail.com>
2024-12-31 16:31:59 +0100

    fixed issues with deployment x3

--- app/auth/signin/page.tsx
+++ app/auth/signin/page.tsx
@@ -1,11 +1,9 @@
-"use server";
-
 import { redirect } from "next/navigation";
 import { SignIn } from "./sign-in";
 
 import { GetUsers } from "@/app/api/queries/getuser-session";
 
-// export const dynamic = "force-dynamic";
+export const dynamic = "force-dynamic";
 
 export default async function Page() {
   const user = await GetUsers();
