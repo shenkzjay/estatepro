b9afd2180c42afc659ce44ed0f8b3a7e72a41fdc
shenkzjay <senksjay@gmail.com>
2025-01-20 11:39:49 +0100

    added loading spinners

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,6 +1,8 @@
 import { DashboardSuspense } from "./dashboard";
 import { Suspense } from "react";
 import { CircleSpinner } from "@/components/app-modals/spinners/circlespinner";
+
+export const dynamic = "force-dynamic";
 export default async function HomeAdminDashboard() {
   return (
     <Suspense fallback={<CircleSpinner />}>
