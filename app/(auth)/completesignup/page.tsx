0eed8d7e5845856873ccfb8507159a79d69affe8
shenkzjay <senksjay@gmail.com>
2025-01-20 12:28:05 +0100

    added loading spinner

--- app/(auth)/completesignup/page.tsx
+++ app/(auth)/completesignup/page.tsx
@@ -1,11 +1,12 @@
 import { CompleteSignUp } from "./complete-signup";
 import { Suspense } from "react";
+import { CircleSpinner } from "@/components/app-modals/spinners/circlespinner";
 
 export default async function Page() {
   return (
     <div className="logbg w-full h-screen flex flex-col justify-center items-center">
       <div className="bg-white p-10 rounded-[32px] md:w-1/3 mx-6 md:mx-0">
-        <Suspense fallback={<div>Loading...</div>}>
+        <Suspense fallback={<CircleSpinner />}>
           <CompleteSignUp />
         </Suspense>
       </div>
